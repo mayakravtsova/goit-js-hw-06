bf6372f042273c850c75fbe0b9cabc35329f5422
Maiia Kravtsova <maiia.kravtsova@gmail.com>
2023-07-16 20:10:13 +0300

    02

---
+++ js/02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const list = document.querySelector("#ingredients");
+let ing = [];
+ingredients.forEach(ingredient => {
+    const element = document.createElement("li");
+    element.classList.add("item");
+    element.textContent = ingredient;
+    console.log(element);
+    ing.push(element);
+});
+
+list.append(...ing);
