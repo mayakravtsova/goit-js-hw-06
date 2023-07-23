da8af54bc0752068111ecbf6d29858ce1bb15047
Maiia Kravtsova <maiia.kravtsova@gmail.com>
2023-07-23 21:35:21 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+      
+    const {
+      elements: { email, password }
+    } = event.currentTarget;
+    
+ //   const formElements = event.currentTarget.elements;
+ //   const email = formElements.email;
+ //   const password = formElements.password;
+ 
+   if (email.value !== "" & password.value !== "") {
+        console.log(`Login: ${email.value}, Password: ${password.value}`);
+    } else {
+        return console.log("Please fill in all the fields!");
+   }
+    
+    event.currentTarget.reset();
+}
