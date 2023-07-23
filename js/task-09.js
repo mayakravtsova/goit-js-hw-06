3b124da6a4a8baa9d48b376b1db88a4ed8aa97b1
Maiia Kravtsova <maiia.kravtsova@gmail.com>
2023-07-23 21:49:49 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const widget = document.querySelector('.widget');
+const backgrColor = widget.querySelector('.color');
+const btn = widget.querySelector('.change-color');
+btn.addEventListener('click', onChangeBackgr);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onChangeBackgr(event) {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  backgrColor.textContent = randomColor;
+};
